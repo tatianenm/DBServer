ccb31aa5d638263dbd847897d6a0a5d78831c97e
Tatiane do Nascimento Mariano <tatiane.mariano@dbserver.com.br>
2019-10-04 16:52:54 -0300

    ajuste codigo

--- src/main/java/com/tatiane/service/RestauranteService.java
+++ src/main/java/com/tatiane/service/RestauranteService.java
@@ -12,29 +12,29 @@ import com.tatiane.repository.RestauranteRepository;
 
 @Service
 public class RestauranteService {
-	
+
 	private Logger logger = LoggerFactory.getLogger(RestauranteService.class);
-	
+
 	private RestauranteRepository restauranteRepository;
 
 	public RestauranteService(RestauranteRepository restauranteRepository) {
 		this.restauranteRepository = restauranteRepository;
 	}
-	
-	public List<Restaurante> findAll(){
-		List<Restaurante> restaurantes= restauranteRepository.findAll();
+
+	public List<Restaurante> findAll() {
+		List<Restaurante> restaurantes = restauranteRepository.findAll();
 		return restaurantes;
 	}
-	
+
 	public Optional<Restaurante> findOne(Integer id) {
-    	logger.debug("find:" + id);
-    	Optional<Restaurante> ent = restauranteRepository.findById(id);
-    	return ent;
-    }
-	
+		logger.debug("find:" + id);
+		Optional<Restaurante> ent = restauranteRepository.findById(id);
+		return ent;
+	}
+
 	public void excluirRestaurante(Integer id) {
 		logger.debug(id + "restaurante removido");
 		restauranteRepository.deleteById(id);
-	}	
+	}
 
 }
