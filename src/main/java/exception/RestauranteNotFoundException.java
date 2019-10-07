2b2e30f3819845d6c4defeab2ab34f0376680d82
Tatiane do Nascimento Mariano <tatiane.mariano@dbserver.com.br>
2019-10-07 11:54:11 -0300

    inclusao arquivo exception restaurante

---
+++ src/main/java/exception/RestauranteNotFoundException.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class RestauranteNotFoundException extends RuntimeException {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	public RestauranteNotFoundException(String message, Throwable cause) {
+		
+	}
+	
+	public RestauranteNotFoundException() {
+		super("Restaurante não encontrado");
+	}
+
+}
